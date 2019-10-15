b29d8ede80391e72851bff69a4906d8ebf9b227c
Kai <mrkaistreet@gmail.com>
2019-10-14 21:16:57 -0400

    Create 9_guessing_game_one.py

---
+++ 9_guessing_game_one.py
@@ -0,0 +1,23 @@
+#Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
+import random
+import sys
+def number_guess():
+  num = random.randinit(1,9)
+  ask_number = int(input('Guess the number: '))
+  if ask_number == num:
+    print('exactly right')
+  elif ask_number < num:
+    print('too low')
+  elif ask_number > num:
+    print('too high')
+  else:
+    sys.exit()
+number_guess()
+play_again = str(input('want to play again? '))
+y = ['y','ya','yea','yeah','yes','yup']
+while play_again in y:
+  number_guess()
+  play_again = str(input('want to play again? '))
+else:
+  print('ok, hope you had fun!')
+  sys.exit()

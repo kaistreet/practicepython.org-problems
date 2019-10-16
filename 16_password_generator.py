5d78c94457fa5f4c6a3eb6d29b89e3d0cb31cef5
Kai <mrkaistreet@gmail.com>
2019-10-15 20:23:28 -0400

    Create 16_password_generator.py

---
+++ 16_password_generator.py
@@ -0,0 +1,13 @@
+#Write a password generator in Python. The passwords should be random, generating a new password every time the user asks for a new password. Include your run-time code in a main method.
+import random
+def password_generator():
+  characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  ask_password_length = int(input('How long do you want your password to be? '))
+  generated_password = "".join(random.sample(characters,ask_password_length))
+  print(generated_password)
+  
+ask_user_password = str(input('Want a random password generated? [enter y for yes or n for no:] '))
+if ask_user_password == 'y':
+  password_generator()
+else:
+  print("alright, i'll let you choose your own password.")

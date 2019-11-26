6e010e7cbe296e79572e96aec9d7667a348cabd7
Kai <mrkaistreet@gmail.com>
2019-11-26 17:53:42 -0500

    Create return_remainder_of_two_numbers.py

---
+++ edabit/return_remainder_of_two_numbers.py
@@ -0,0 +1,5 @@
+#There is a single operator in Python, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+larger_num = int(input('Enter larger number: '))
+smaller_num = int(input('Enter smaller number: '))
+remainder = larger_num % smaller_num
+print(remainder)

a4a554a6687ee96f940dcd45c7706771feb34914
Kai <mrkaistreet@gmail.com>
2019-11-26 12:58:21 -0500

    Update and rename test.py to chapter_1.py

---
+++ introducing-python-problems/chapter_1.py
@@ -0,0 +1,8 @@
+#Problem 1.3: Use the interpreter like a calculator and type 8 * 9. Press Enter to see the result.
+print(8*9)
+
+#Problem 1.4: Type the number 47 and press Enter. Did that print 47 to the next line?
+47
+
+#Problem 1.5: Type print(47) and press Enter. Did that print 47 to the next line?
+print(47)

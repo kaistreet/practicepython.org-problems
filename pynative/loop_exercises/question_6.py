92ca09c6d28ee2e60dc33b8bc902172a47aac189
Kai <mrkaistreet@gmail.com>
2020-03-13 23:27:13 -0700

    Create question_6.py

---
+++ pynative/loop_exercises/question_6.py
@@ -0,0 +1,3 @@
+#Question 6: Given a number count the total number of digits in a number
+ask_user_number = int(input('Enter a number and receive a count of its digits: '))
+print(str('count is equal to: ')+str(len(str(ask_user_number))))

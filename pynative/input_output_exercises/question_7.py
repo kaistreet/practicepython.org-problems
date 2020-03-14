0947e07fa8a1037bd088faaf3a8f17b894e4354f
Kai <mrkaistreet@gmail.com>
2020-03-13 22:56:20 -0700

    Create question_7.py

---
+++ pynative/input_output_exercises/question_7.py
@@ -0,0 +1,5 @@
+#Question 7: Accept any three string from one input() call
+ask1, ask2, ask3 = input('Enter three strings: ').split(" ")
+print(str('first string is: ')+ask1)
+print(str('second string is: ')+ask2)
+print(str('third string is: ')+ask3)

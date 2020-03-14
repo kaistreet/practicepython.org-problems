21f559af601c82d14212f600d389f0a861fada67
Kai <mrkaistreet@gmail.com>
2020-03-14 12:49:41 -0700

    Create question_3.py

---
+++ pynative/list exercises/question_3.py
@@ -0,0 +1,6 @@
+#Question 3: Given a Python list. Turn every item of a list into its square root
+a_list = [1, 2, 3, 4, 5, 6, 7]
+sq_list = []
+for num in a_list:
+	sq_list.append(num*num)
+print(sq_list)

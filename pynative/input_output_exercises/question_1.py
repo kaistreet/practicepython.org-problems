d8bed4efa1edc53b715ed21747638232427c3634
Kai <mrkaistreet@gmail.com>
2020-03-13 22:55:12 -0700

    Update and rename pynative/question_1.py to pynative/input_output_exercises/question_1.py

---
+++ pynative/input_output_exercises/question_1.py
@@ -0,0 +1,4 @@
+#Question 1: Accept two numbers from the user and calculate multiplication
+ask_user_num = int(input('Enter a number: '))
+ask_user_num_2 = int(input('Enter another number: '))
+print(str(ask_user_num)+str(' multiplied by ')+str(ask_user_num_2)+' equals '+str(ask_user_num*ask_user_num_2))
--- pynative/question_1.py
+++
@@ -1,5 +0,0 @@
-#Accept two int values from the user and return their product. If the product is greater than 1000, then return their sum
-ask_number_1 = int(input('Enter first integer: '))
-ask_number_2 = int(input('Enter the second integer: '))
-result = ask_number_1*ask_number_2
-print(result)

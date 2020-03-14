a055370bfc54d687dd89a29d448257af8202ad83
Kai <mrkaistreet@gmail.com>
2020-03-13 23:27:26 -0700

    Create question_8.py

---
+++ pynative/loop_exercises/question_8.py
@@ -0,0 +1,6 @@
+#Question 8: Reverse the following list using for loop
+num_list = [10,20,30,40,50]
+reverse_num_list = num_list
+reverse_num_list.reverse()
+for num in reverse_num_list:
+	print(num)

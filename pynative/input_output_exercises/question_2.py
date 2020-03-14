7da05964bbfaad4b0dc1c76661da52a1c5fc8215
Kai <mrkaistreet@gmail.com>
2020-03-13 22:55:32 -0700

    Create question_2.py

---
+++ pynative/input_output_exercises/question_2.py
@@ -0,0 +1,3 @@
+#Question 2: Display “My Name Is James” as “My**Name**Is**James” using output formatting of a print() function
+name_string = 'My Name Is James'
+print(name_string.replace(" ","**"))
